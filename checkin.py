cceca448fe5c327ccb1146d4124ba89808e7a639
Carey036 <15318188117@189.cn>
2020-10-11 19:04:47 +0800

    增加server酱发送消息

--- checkin.py
+++ checkin.py
@@ -9,9 +9,12 @@ password = ""
 if(username == "" or password == ""):
     username = input("账号：")
     password = input("密码：")
+    pushkey = input("pushkey：")
 
 def main():
+    pushmsg = ""
     login(username, password)
+    pushmsg += f"user:{username}\n\n"
     rand = str(round(time.time()*1000))
     surl = f'https://api.cloud.189.cn/mkt/userSign.action?rand={rand}&clientType=TELEANDROID&version=8.6.3&model=SM-G930K'
     url = f'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action?taskId=TASK_SIGNIN&activityId=ACT_SIGNIN'
@@ -26,8 +29,10 @@ def main():
     netdiskBonus = response.json()['netdiskBonus']
     if(response.json()['isSign'] == "false"):
         print(f"未签到，签到获得{netdiskBonus}M空间")
+        pushmsg += f"未签到，签到获得{netdiskBonus}M空间\n\n"
     else:
         print(f"已经签到过了，签到获得{netdiskBonus}M空间")
+        pushmsg += f"已经签到过了，签到获得{netdiskBonus}M空间\n\n"
     headers = {
         'User-Agent':'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
         "Referer" : "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
@@ -37,15 +42,22 @@ def main():
     response = s.get(url,headers=headers)
     if ("errorCode" in response.text):
         print(response.text)
+        pushmsg += response.text + "\n\n"
     else:
         description = response.json()['description']
         print(f"抽奖获得{description}")
+        pushmsg += f"抽奖获得{description}\n\n"
     response = s.get(url2,headers=headers)
     if ("errorCode" in response.text):
         print(response.text)
+        pushmsg += response.text + "\n\n"
     else:
         description = response.json()['description']
         print(f"抽奖获得{description}")
+        pushmsg += f"抽奖获得{description}\n\n"
+    #print(pushmsg)
+    pushdata = {"text":f"{username}天翼网盘打卡抽奖","desp":pushmsg}
+    s.post("https://sc.ftqq.com/SCU56081T354e97b00e50fb6e49baea6322553bbd5f82dfeb31777.send",data=pushdata)
 
 BI_RM = list("0123456789abcdefghijklmnopqrstuvwxyz")
 def int2char(a):
